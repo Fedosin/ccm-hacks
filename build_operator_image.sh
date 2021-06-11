65e24ba1330092903abf3d7c7b600561a5e31c9f
Mike Fedosin <mfedosin@redhat.com>
2021-06-11 12:00:03 +0200

    Rework build_operator_image.sh script

--- build_operator_image.sh
+++ build_operator_image.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -ex
+
 help() {
     echo "Build an operator image with custom changes to support external cloud providers"
     echo ""
@@ -7,6 +9,7 @@ help() {
     echo "Options:"
     echo "-h, --help      show this message"
     echo "-o, --operator  operator name to build, available options: mco, kapio, kcmo"
+    echo "-i, --id        id of your pull request to apply on top of the master branch"
     echo "-u, --username  registered username in quay.io"
     echo "-t, --tag       push to a custom tag in your origin release image repo, default: latest"
     echo ""
@@ -15,63 +18,78 @@ help() {
 TAG="latest"
 
 # Parse Options
-case $1 in
-    -h|--help)
-        help
-        exit 0;;
-    -t|--tag)
-        TAG=$2
-        shift
-        shift;;
-    -u|--username)
-        USERNAME=$2
-        shift 2
-        ;;
-    -o|--operator)
-        OPERATOR=$2
-        shift 2
-        ;;
-    *);;
-esac
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        -h|--help)
+            help
+            exit 0
+            ;;
+
+        -u|--username)
+            USERNAME=$2
+            shift 2
+            ;;
+
+        -t|--tag)
+            TAG=$2
+            shift 2
+            ;;
+
+        -o|--operator)
+            OPERATOR_NAME=$2
+            shift 2
+            ;;
+
+        -i|--id)
+            PRID=$2
+            shift 2
+            ;;
+
+        *)
+            echo "Invalid option $1"
+            help
+            exit 0
+            ;;
+    esac
+done
 
 if [ -z "$USERNAME" ]; then
     echo "No quay.io username provided, exiting ..."
     exit 1
 fi
 
-if [ -z "$OPERATOR" ]; then
+if [ -z "$OPERATOR_NAME" ]; then
     echo "No operator name provided, exiting ..."
     exit 1
 fi
 
-case $OPERATOR in
+OPERATOR_IMAGE=quay.io/$USERNAME/$OPERATOR_NAME:$TAG
+GITHUB_REPO="https://github.com/openshift/$OPERATOR_NAME"
 
-  mco)
-    OPERATOR_IMAGE=quay.io/$USERNAME/machine-config-operator:$TAG
-    ;;
+git ls-remote $GITHUB_REPO 1>/dev/null
 
-  kapio)
-    OPERATOR_IMAGE=quay.io/$USERNAME/cluster-kube-apiserver-operator:$TAG
-    ;;
+echo "Cloning repo $GITHUB_REPO"
+rm -rf $OPERATOR_NAME
+git clone $GITHUB_REPO
 
-  kcmo)
-    OPERATOR_IMAGE=quay.io/$USERNAME/cluster-kube-controller-manager-operator:$TAG
-    ;;
+pushd $OPERATOR_NAME
 
-  *)
-    echo -n "unknown operator image name"
-    exit 1
-    ;;
-esac
+echo "Applying your changes"
+git fetch origin pull/$PRID/head:changes
+git checkout changes
+git rebase master
 
 echo "Setting operator image to $OPERATOR_IMAGE"
 
-set -ex
-
 echo "Start building operator image"
-podman build --no-cache -t $OPERATOR_IMAGE -f Dockerfile.$OPERATOR
+podman build --no-cache -t $OPERATOR_IMAGE .
 
 echo "Pushing operator image to quay.io"
 podman push $OPERATOR_IMAGE
 
+popd
+
 echo "Successfully pushed $OPERATOR_IMAGE"
+
+echo "Cleaning up"
+rm -rf $OPERATOR_NAME
